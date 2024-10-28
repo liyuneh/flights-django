824f6dfcb035844ae53bae68b58867491664eb21
Liyuneh <liyurstey@gmail.com>
2024-10-28 22:54:44 +0300

    testing

--- apps/admin.py
+++ apps/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Airport, Flight,Passenger,Food
 
 admin.site.register(Airport)
-admin.site.register(Flight)
 admin.site.register(Passenger)
 admin.site.register(Food)
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ['id', 'origin', 'destination', 'duration']
 # Register your models here.
--- apps/views.py
+++ apps/views.py
@@ -7,8 +7,11 @@ from .models import Airport, Flight, Passenger,Food
 
 
 def index(request):
+     flights = Flight.objects.all()
+     print(flights)
+
      context={
-         "flight":Flight.objects.all()
+         "flight":flights
      }
      return render(request, 'apps/index.html', context)
 
@@ -29,18 +32,34 @@ def flight(request, flight_id):
      return render(request, 'apps/flight.html', context)
 
 def book(request, flight_id):
-    try:
-        passenger_id = int(request.POST["passenger"])
-        passenger = Passenger.objects.get(pk=passenger_id)
-        flight = Flight.objects.get(pk=flight_id)
-    except KeyError:
-        return render(request, 'apps/error.html', {'Message': "No Selection."})
-    except Flight.DoesNotExist:
-        return render(request, 'apps/error.html', {'Message': "No flight."})
-    except Passenger.DoesNotExist:
-        return render(request, 'apps/error.html', {'Message': "No passenger."})
-    passenger.flights.add(flight)
-    return HttpResponseRedirect(reverse('flight',args=(flight.id,) )) 
+    if request.method=='POST':
+        try:
+            passenger_id = int(request.POST["passenger"])
+            passenger = Passenger.objects.get(pk=passenger_id)
+            flight = Flight.objects.get(pk=flight_id)
+            action = request.POST.get("action")  
+
+            print("info:", flight_id, passenger, flight, action)
+            if action == 'add':
+                passenger.flights.add(flight)
+                flight.save()
+                print(f"book person {passenger} added to flight {flight}.")
+            elif action == 'remove':
+                passenger.flights.remove(flight)
+                flight.save()
+                print(f"flight person {passenger} removed from flight {flight}.")
+        except KeyError:
+            return render(request, 'apps/error.html', {'Message': "No Selection."})
+        except Flight.DoesNotExist:
+            return render(request, 'apps/error.html', {'Message': "No flight."})
+        except Passenger.DoesNotExist:
+            return render(request, 'apps/error.html', {'Message': "No passenger."})
+        return HttpResponseRedirect(reverse('flight',args=(flight.id,) )) 
+    else:
+        print(f"invalid request!")
+        return render(request, 'apps/error.html', {'Message': "Invalid request method."})
+        
+
 
 def food(request, flight_id):
     if request.method == 'POST':
@@ -49,23 +68,33 @@ def food(request, flight_id):
             food_id = int(request.POST["food"])
             food = Food.objects.get(pk=food_id)
             flight = Flight.objects.get(pk=flight_id)
-            print( "info:", food_id, food, flight)
-            # Associate the food item with the flight
-            food.flights.add(flight)
+            action = request.POST.get("action")  
 
-        except KeyError  as e:
+            print("info:", food_id, food, flight, action)
+
+            
+            if action == 'add':
+            
+                food.flights.add(flight)
+                flight.save()
+                print(f"Food item {food} added to flight {flight}.")
+            elif action == 'remove':
+                # Remove the food item from the flight
+                food.flights.remove(flight)
+                flight.save()
+                print(f"Food item {food} removed from flight {flight}.")
+
+        except KeyError as e:
             print("KeyError:", e)
             return render(request, 'apps/error.html', {'Message': "No food selection made."})
-        except Food.DoesNotExist  as e:
+        except Food.DoesNotExist as e:
             print("DoesNotExist:", e)
             return render(request, 'apps/error.html', {'Message': "No food item found."})
-        except Flight.DoesNotExist  as e:
+        except Flight.DoesNotExist as e:
             print("error3:", e)
-            
             return render(request, 'apps/error.html', {'Message': "No flight found."})
         except Exception as e:
             print("error4:", e)
-            
             return render(request, 'apps/error.html', {'Message': str(e)})
 
         return HttpResponseRedirect(reverse('flight', args=(flight.id,)))
@@ -73,3 +102,4 @@ def food(request, flight_id):
     # If the request method is not POST
     else:
         return render(request, 'apps/error.html', {'Message': "Invalid request method."})
+
